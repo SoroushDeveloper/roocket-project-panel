d379f5f2f8fe7049394d9440e555776b02a2c823
SoroushDev <mr.soroosh.qr@gmail.com>
2024-08-14 19:12:29 +0330

    Fixed Login Page
    Took 12 minutes

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -6,6 +6,7 @@ import {useAppDispatch} from "@/app/hooks";
 import {updateVerifyToken} from "@/app/store/auth";
 import GuestPanelLayout from "@/app/components/guestPanelLayout";
 import Cookies from "universal-cookie";
+import Hr from "@/app/components/shared/hr";
 
 const Login: NextPageWithLayout = () => {
     const dispatch = useAppDispatch();
@@ -22,7 +23,7 @@ const Login: NextPageWithLayout = () => {
 
     return (
         <>
-            <section className="bg-gray-50 dark:bg-gray-900">
+            <section className="bg-gray-50 dark:bg-gray-900 h-screen sm:w-screen">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <Image className="w-8 h-8 mr-2" width="60" height="60"
@@ -32,9 +33,10 @@ const Login: NextPageWithLayout = () => {
                     <div
                         className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            <h1 className="text-xl text-center sm:text-start font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Login to your account
                             </h1>
+                            <Hr/>
                             <LoginForm setToken={setVerifyToken}/>
                         </div>
                     </div>
