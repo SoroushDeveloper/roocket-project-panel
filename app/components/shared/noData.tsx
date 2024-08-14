39807f818edb61d086242b62c977fa9a88580084
SoroushDev <mr.soroosh.qr@gmail.com>
2024-08-14 18:51:39 +0330

    NoData Message Theme Fixed
    Took 1 minute

--- app/components/shared/noData.tsx
+++ app/components/shared/noData.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function NoData() {
     return (
         <>
-            <div className="text-center bg-gray-800 p-5 mt-5 rounded flex justify-center">
+            <div className="text-center bg-gray-200 dark:bg-gray-800 p-5 mt-5 rounded flex justify-center">
                 <ExclamationCircleIcon className="flex-shrink-0 h-6 w-6"/>
                 <h2 className="ml-2">
                     No Data Available
