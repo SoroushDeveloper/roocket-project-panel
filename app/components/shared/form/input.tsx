d97d20ac288d2930c574ecac02acf1135f892f9e
SoroushDev <mr.soroosh.qr@gmail.com>
2024-08-14 19:18:10 +0330

    Fixed Mobile Login Page
    Took 6 minutes

--- app/components/shared/form/input.tsx
+++ app/components/shared/form/input.tsx
@@ -20,11 +20,12 @@ const Input: FC<InputProps> = ({
                                }) => {
     return (
         <>
-            <label htmlFor={name} className={`block text-sm font-medium text-gray-700 dark:text-gray-300 ${labelClassName ?? ''}`}>
+            <label htmlFor={name}
+                   className={`block text-sm font-medium text-gray-700 dark:text-gray-300 ${labelClassName ?? ''}`}>
                 {label}
             </label>
             <Field id={name} name={name} type={type}
-                   className={`mt-1 bg-gray-100 dark:bg-gray-900 appearance-none block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${inputClassName ?? ''}`}/>
+                   className={`mt-1 bg-gray-100 dark:bg-gray-900 appearance-none block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-sm ${inputClassName ?? ''}`}/>
             <ErrorMessage name={name} className={`text-red-500 text-sm ${errorClassName ?? ''}`} component="div"/>
         </>
     );
