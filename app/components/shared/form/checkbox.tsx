2ad4c9e9314c143bfb3233815f0b1731168f2a98
Programmer01 <snb.programmer001@gmail.com>
2024-08-24 11:46:50 +0330

    Fix Checkbox

--- app/components/shared/form/checkbox.tsx
+++ app/components/shared/form/checkbox.tsx
@@ -18,7 +18,7 @@ const Checkbox: FC<CheckboxProps> = ({
                                }) => {
     return (
     <>
-    <div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+    <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
         <Field id={name} name={name} type="checkbox"
                 className={`w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 ${inputClassName ?? ''}`}/>     
         <label htmlFor={name} className={`w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 ${labelClassName ?? ''}`}>
