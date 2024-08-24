f73ea603f3284131d58180b52cdfe6f1f3182336
Programmer01 <snb.programmer001@gmail.com>
2024-08-24 09:49:34 +0330

    Fix

--- app/components/panel/articles/innerCreateArticleForm.tsx
+++ app/components/panel/articles/innerCreateArticleForm.tsx
@@ -84,11 +84,9 @@ const InnerCreateArticleForm = (props: FormikProps<ArticleFormValuesInterface>) 
                     </div>
                 </div>
                 <div className="grid gap-6 mb-6 md:grid-cols-1">
-                    <Editor content={
-                        <textarea name="content" id="content" rows={4}
-                                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  placeholder="Write your thoughts here..."></textarea>
-                    }/>
+                <textarea name="content" id="content" rows={4}
+                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Write your thoughts here..."></textarea>
                 </div>
                 <div className="grid gap-6 mb-6 md:grid-cols-1">
                     <div>
