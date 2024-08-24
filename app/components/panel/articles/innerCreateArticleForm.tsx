6a3e087038f4b5e5660ca5be6fc1bfb6df4f31d8
Programmer01 <snb.programmer001@gmail.com>
2024-08-24 10:54:22 +0330

    Create Article

--- app/components/panel/articles/innerCreateArticleForm.tsx
+++ app/components/panel/articles/innerCreateArticleForm.tsx
@@ -1,4 +1,4 @@
-import {Form, FormikProps} from "formik";
+import {Field, Form, FormikProps} from "formik";
 
 import Input from "@/app/components/shared/form/input";
 import {ArticleFormValuesInterface} from "@/app/contracts/panel";
@@ -8,7 +8,7 @@ import callApi from "@/app/helpers/callApi";
 import Fail from "@/app/components/toasts/fail";
 import Cookies from "universal-cookie";
 import {CheckCircleIcon} from "@heroicons/react/24/solid";
-import Editor from "@/app/components/shared/editor";
+import Textarea from "../../shared/form/textarea";
 
 const InnerCreateArticleForm = (props: FormikProps<ArticleFormValuesInterface>) => {
     const cookie = new Cookies;
@@ -46,49 +46,29 @@ const InnerCreateArticleForm = (props: FormikProps<ArticleFormValuesInterface>) 
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                             Category
                         </label>
-                        <select id="categories" name="category_id"
-                                className="bg-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected>Choose a category</option>
+                        <Field id="categories" name="category_id" as='select'
+                        className="bg-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option>Choose a category</option>
                             {categories.map((category, key) =>
                                 <option key={key} value={category.id}>{category.title}</option>)}
-                        </select>
+                        </Field>
                     </div>
                 </div>
                 <div className="grid gap-6 mb-6 md:grid-cols-3">
                     <div>
-                        <label htmlFor="keywords"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Keywords
-                        </label>
-                        <textarea id="keywords" rows={4} name="keywords"
-                                  className="resize-none block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  placeholder="Separate with commas"></textarea>
+                        <Textarea name="keywords" label="Keywords"/>
                     </div>
                     <div>
-                        <label htmlFor="description"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Description
-                        </label>
-                        <textarea id="description" rows={4} name="description"
-                                  className="resize-none block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  placeholder="Write a description for this article"></textarea>
+                        <Textarea name="description" label="Description"/>
                     </div>
                     <div>
-                        <label htmlFor="summary"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Summary
-                        </label>
-                        <textarea id="summary" rows={4} name="summary"
-                                  className="resize-none block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  placeholder="Write a summary about this article"></textarea>
+                        <Textarea name="summary" label="Summary"/>
                     </div>
                 </div>
-                <div className="grid gap-6 mb-6 md:grid-cols-1">
-                <textarea name="content" id="content" rows={4}
-                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Write your thoughts here..."></textarea>
+                <div  className="grid gap-6 mb-6 md:grid-cols-1">
+                    <Textarea name="content" label="Content"/>
                 </div>
-                <div className="grid gap-6 mb-6 md:grid-cols-1">
+                <div  className="grid gap-6 mb-6 md:grid-cols-1">
                     <div>
                         <Input name="image_url" label="Image URL"/>
                     </div>
