983802b77de9e3fcf45a367e3f1de4b21b016277
SoroushDeveloper <mr.soroosh.qr@gmail.com>
2024-09-04 13:27:04 +0330

    Fixed Navbar items hover in light mode

--- app/components/sidebar/items.tsx
+++ app/components/sidebar/items.tsx
@@ -18,7 +18,7 @@ export default function SidebarItems({children, classNames = '', name, link, as 
         <>
             <li>
                 <Link href={link} onClick={hideSidebar}
-                      className={`${classNames} flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${active ? 'bg-blue-500 hover:bg-blue-600' : 'hover:bg-gray-100 dark:hover:bg-gray-700'}`}>
+                      className={`${classNames} flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${active ? 'bg-blue-500 hover:bg-blue-600' : 'hover:bg-gray-300 dark:hover:bg-gray-700'}`}>
                     {children}
                     <span className="ms-3">
                         {name}
