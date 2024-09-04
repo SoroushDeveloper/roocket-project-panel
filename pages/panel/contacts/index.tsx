2d0c7d2f60f53740ad37abab33dea5b4e9c78a54
SoroushDeveloper <mr.soroosh.qr@gmail.com>
2024-09-04 19:17:41 +0330

    Fixed StatusRadios Default Checked

--- pages/panel/contacts/index.tsx
+++ pages/panel/contacts/index.tsx
@@ -61,7 +61,7 @@ const Contacts: NextPageWithLayout = () => {
                     <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 pr-3">
                         <div className="flex items-center ps-3">
                             <input id="horizontal-list-radio-license" type="radio" value="all" name="status" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                   onChange={(e) => setStatus('')}/>
+                                   onChange={(e) => setStatus('')} checked={status == ''}/>
                             <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                 All
                             </label>
@@ -70,7 +70,7 @@ const Contacts: NextPageWithLayout = () => {
                     <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 pr-3">
                         <div className="flex items-center ps-3">
                             <input id="horizontal-list-radio-id" type="radio" value="pending" name="status" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                   onChange={(e) => setStatus('0')}/>
+                                   onChange={(e) => setStatus('0')} checked={status == '0'}/>
                             <label htmlFor="horizontal-list-radio-id" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                 Pending
                             </label>
@@ -79,7 +79,7 @@ const Contacts: NextPageWithLayout = () => {
                     <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 pr-3">
                         <div className="flex items-center ps-3">
                             <input id="horizontal-list-radio-military" type="radio" value="reviewed" name="status" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                   onChange={(e) => setStatus('1')}/>
+                                   onChange={(e) => setStatus('1')} checked={status == '1'}/>
                             <label htmlFor="horizontal-list-radio-military" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                 Reviewed
                             </label>
