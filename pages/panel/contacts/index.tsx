54d13febec4f6611d9195140e551122436d9a782
SoroushDeveloper <mr.soroosh.qr@gmail.com>
2024-09-04 19:14:39 +0330

    Fixed Contact StatusRadios on mobile

--- pages/panel/contacts/index.tsx
+++ pages/panel/contacts/index.tsx
@@ -53,11 +53,11 @@ const Contacts: NextPageWithLayout = () => {
     }
     return (
         <div>
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col sm:flex-row justify-between items-center">
                 <h1 className="text-4xl">
                     Contacts
                 </h1>
-                <ul className="items-center text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                <ul className="flex mt-2 sm:mt-0 items-center text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                     <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 pr-3">
                         <div className="flex items-center ps-3">
                             <input id="horizontal-list-radio-license" type="radio" value="all" name="status" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
