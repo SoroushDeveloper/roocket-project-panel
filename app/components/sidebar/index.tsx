67e713b143d314372b2d70424ded2f7402a781c5
Programmer01 <snb.programmer001@gmail.com>
2024-09-09 10:52:21 +0330

    Fixed Sidebar Buttons

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -44,20 +44,21 @@ export default function Sidebar() {
                           d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
             </button>
-            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"
-                    type="button" onClick={showSidebarHandler} hidden={showSidebar}
+            <button type="button" onClick={logoutHandler}
                     className={`inline-flex float-end items-center p-2 mt-2 me-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ${showSidebar ? 'opacity-0' : ''}`}>
-                <span className="sr-only">Open sidebar</span>
+                <span className="sr-only">Logout</span>
                 <PowerIcon className="flex-shrink-0 h-6 w-6"/>
             </button>
             <aside id="logo-sidebar"
                    className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform shadow-2xl shadow-gray-100 dark:shadow-gray-900 sm:shadow-none ${!showSidebar ? '-translate-x-full' : ''} sm:translate-x-0`}
                    aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100 dark:bg-gray-900 sm:mt-4 sm:ml-2 rounded">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100 dark:bg-gray-900 sm:mt-4 sm:ml-2 sm:rounded">
                     <div className="flex justify-start items-center mb-5">
-                        <button type="button" onClick={logoutHandler}
+                        <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar"
+                                aria-controls="logo-sidebar"
+                                type="button" onClick={showSidebarHandler}
                                 className="inline-flex items-center me-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                            <span className="sr-only">Logout</span>
+                            <span className="sr-only">Open sidebar</span>
                             <XMarkIcon className="flex-shrink-0 h-6 w-6"/>
                         </button>
                         <Link href="/panel"
