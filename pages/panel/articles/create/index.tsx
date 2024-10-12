510333a2379f9089d757c1cf5e3813fa3aa2fa6c
SoroushDeveloper <mr.soroosh.qr@gmail.com>
2024-10-12 19:35:48 +0330

    Remove the obsolete back button from the EditArticle page

--- pages/panel/articles/create/index.tsx
+++ pages/panel/articles/create/index.tsx
@@ -14,11 +14,6 @@ const CreateArticle: NextPageWithLayout = () => {
                 <h1 className="text-4xl">
                     Create New Article
                 </h1>
-                <Link href="/panel/articles"
-                      className="mt-3 sm:mt-0 rounded flex items-center bg-none border-2 border-gray-500 p-2 text-gray-500 hover:bg-gray-500 hover:text-gray-100 hover:dark:text-gray-900">
-                    <ChevronLeftIcon className="flex-shrink-0 h-5 w-5 mr-2"/>
-                    Back to Articles
-                </Link>
             </div>
             <Hr my={true}/>
             <CreateArticleForm/>
