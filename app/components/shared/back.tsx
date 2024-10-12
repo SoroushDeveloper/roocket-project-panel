ed0ca0474a15a9a4b9bd1dcfc89c023c193774e4
SoroushDeveloper <mr.soroosh.qr@gmail.com>
2024-10-12 19:21:29 +0330

    Some tweaks on the back button

--- app/components/shared/back.tsx
+++ app/components/shared/back.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Back() {
     return (
         <button onClick={() => router.back()}
-                className="fixed bottom-4 left-3 bg-red-500 rounded-full shadow-2xl shadow-black p-2 sm:hidden">
+                className="fixed bottom-4 left-4 bg-red-500 rounded-full shadow-2xl shadow-black p-3 sm:hidden">
             <ArrowLeftIcon className="h-6 w-6"/>
         </button>
     )
